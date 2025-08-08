523e8deea0b59064602bd355548f13f0f598f461
Joseph Juma <57210731+joseph-gitau@users.noreply.github.com>
2025-08-08 11:28:13 +0300

    Update Login.jsx

--- athletics-boosters/src/pages/Login.jsx
+++ athletics-boosters/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login()
     const handleSubmit = async (e) =>
     {
         e.preventDefault();
-        const res = await fetch('http://localhost:5000/api/login', {
+        const res = await fetch('https://morgantown-boosters-t3c2.vercel.app/api/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(form),
